1e3e0f016c1f7deb75f975c1fcfd806ff6bfb18b
Aniowo Chinedu <aniowochinedu@gmail.com>
2025-01-27 12:16:58 +0100

    Initial Commit

---
+++ src/app/grade-management/add-grade/add-grade.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-grade',
+  imports: [],
+  templateUrl: './add-grade.component.html',
+  styleUrl: './add-grade.component.css'
+})
+export class AddGradeComponent {
+
+}
---
+++ src/app/grade-management/grade-list/grade-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grade-list',
+  imports: [],
+  templateUrl: './grade-list.component.html',
+  styleUrl: './grade-list.component.css'
+})
+export class GradeListComponent {
+
+}
